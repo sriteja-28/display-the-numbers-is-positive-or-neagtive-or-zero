9a9f890d13746dc8d12314a35e80ff612299109e
sriteja-28 <108087209+sriteja-28@users.noreply.github.com>
2022-06-24 12:34:55 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+n=int(input("enter a number"))
+if n==0:
+    print("the given nnumber is zero")
+elif n>1:
+    print("the given number is positive")
+else:
+    print("the given number is negative")
+
+    
+
+    
